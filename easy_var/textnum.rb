60ee197617d066157f35d5af095550e1cb11efd3
pavloolenchyk <thetankinator47@gmail.com>
2019-02-24 10:18:12 +0900

    textnum.rb edited
    ready to go

--- easy_var/textnum.rb
+++ easy_var/textnum.rb
@@ -4,5 +4,6 @@ puts "Enter number of pages in book:"
 pages = gets
 puts "Enter number of pages you have read:"
 read = gets
-percentage = read / pages
-puts "You have read" + percentage + "of the book."
+percentage = read.to_f / pages.to_f
+actual = percentage * 100
+puts "You have read " + actual.to_s + " % " + "of the book."
