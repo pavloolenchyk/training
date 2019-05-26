fd2277397beca93e14167c51cb038ee0f2c66ec8
pavloolenchyk <thetankinator47@gmail.com>
2019-05-26 09:32:30 +0900

    Inheritance of traits from classes in Ruby
    Made a file, classes.rb, that shows inheritance between the Mammal, Primate, and Human classes.

---
+++ ruby_classes/classes.rb
@@ -0,0 +1,17 @@
+class Mammal
+def breathe
+puts "inhale, exhale"
+end
+end
+
+class Primate < Mammal
+def eat()
+puts "chomp"
+end
+end
+
+class Human < Primate
+def talk
+puts "Hello World!"
+end
+end
