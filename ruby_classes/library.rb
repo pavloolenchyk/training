3d9a0be6a7295b0a0b99f25c4abdfe62826fbb5d
pavloolenchyk <thetankinator47@gmail.com>
2020-01-11 13:28:52 +0900

    Temporary copy of library.rb created
    After a disastrous git reset --hard, I salvaged what I could and made a new library file from scratch, identical to the previous one.

---
+++ ruby_classes/library.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+class Book
+  def initialize(genre)
+    @genre = genre
+  end
+  def genre
+    return(@genre)
+  end
+end
+
+class Fiction < Book
+  def initialize
+    super "fiction"
+  end
+end
+
+book = Fiction.new
+
+puts(book.genre)
