38de2c65231c579bc7813e4a8cd10ebeee60298e
pavloolenchyk <thetankinator47@gmail.com>
2020-01-26 10:25:31 +0900

    Робота з бібліотеками
    Створено нову директорію lib, щоб тримати класи fiction.rb і book.rb. Також перенесено файли dolphin.rb і monkey.rb у директорію lib і змінено файл literature.rb щоб використати require між файлами.

--- ruby_classes/literature.rb
+++ ruby_classes/literature.rb
@@ -1,20 +1,6 @@
 #!/usr/bin/ruby
 
-
-class Book
-  def initialize(genre)       # initialize - метод, який створює об'єкт. genre - параметер, в який вносится значення об'єкту.
-    @genre = genre            # @genre - змінна екземпляру. в якого ми вносемо значення із попереднього параметру genre.
-  end
-  def genre
-    return(@genre)
-  end
-end
-
-class Fiction < Book
-  def initialize
-    super "fiction"           # "fiction" - виклик того самого методу, який був у батьківському класу.
-  end
-end
+require_relative './lib/fiction'
 
 book = Fiction.new
 
