d3b05fd481d40068b8d80e4e48c5e69732ef6326
pavloolenchyk <thetankinator47@gmail.com>
2020-03-22 10:19:11 +0900

    Змінено файл factorial.rb
    Змінено файл, щоб він використовував функції і аргументи.

--- control_structures/factorial.rb
+++ control_structures/factorial.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 
-factorial = 1
 puts "Введіть цифру, яку хочете застосувати:"
 num = gets.chomp.to_i
-i = 1
 
-(i..num).each do |i|
-  factorial = factorial * i
+def factorial(num)
+  product = 1
+  (1..num).each do |i|
+    product = product * i
+  end
+  return product
 end
 
-puts factorial
+puts factorial(num)
